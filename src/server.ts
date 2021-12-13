2939d48448ff5c537acbb0fd68cda9bd29d89489
Birk Brauer <3842623+bb1@users.noreply.github.com>
2021-12-13 10:51:23 +0100

    prepare code 4 backend challenge

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,13 @@
-// TODO: Express web server
 
-const baseUrl = 'https://geoportal.stadt-koeln.de/Finder/Lookup?filter=type:adr&query=';
+interface Address {
+    street: string;
+    zip: string;
+    number: string;
+    district: string;
+}
+
+const BASE_URL = 'https://geoportal.stadt-koeln.de/Finder/Lookup?filter=type:adr&query=';
+
+export function autoCompleteAddress(query): Address[] {
+    return []
+}
