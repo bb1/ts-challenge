96a082e95508cf1076baa521eab556145914652f
Birk Brauer <3842623+bb1@users.noreply.github.com>
2021-12-13 15:09:45 +0100

    added AutoCompleteResponse

--- src/server.test.ts
+++ src/server.test.ts
@@ -3,16 +3,20 @@ import { autoCompleteAddress, Address } from './server';
 const QUERY_404 = 'you_not_gonna_find_anything_with_this';
 
 test('basic type structure', () => {
-    const queryJune = autoCompleteAddress(QUERY_404);
-    expect(queryJune).toBeInstanceOf(Array);
     expect(typeof autoCompleteAddress).toEqual('function');
+    const queryJune = autoCompleteAddress(QUERY_404);
+    expect(queryJune).toBeInstanceOf(Object);
+    expect(Number.isInteger(queryJune.count)).toBeTruthy();
+    expect(queryJune.addresses).toBeInstanceOf(Array);
+    expect(typeof queryJune.time).toBe('number');
 });
 
 test('find strasse', () => {
     const queryJune = autoCompleteAddress("strasse");
-    expect(queryJune).toBeInstanceOf(Array);
-    expect(queryJune.length).toEqual(1);
-    expect(queryJune[0]).toMatchObject<Address>({
+    const addresses = queryJune.addresses;
+    expect(addresses).toBeInstanceOf(Array);
+    expect(addresses.length).toEqual(1);
+    expect(addresses[0]).toMatchObject<Address>({
         district: 'Kalk',
         zip: '51103',
         street: 'Straße des 17. Juni',
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,10 @@
 import fetch from 'node-fetch';
 
+export interface AutoCompleteResponse {
+    count: number;
+    addresses: Address[];
+    time: number;
+}
 export interface Address {
     street: string;
     numbers: string[];
@@ -9,6 +14,6 @@ export interface Address {
 
 const BASE_URL = 'https://geoportal.stadt-koeln.de/Finder/Lookup?filter=type:adr&query=';
 
-export function autoCompleteAddress(query: string): Address[] {
-    return []
+export function autoCompleteAddress(query: string): AutoCompleteResponse {
+    return {};
 }
