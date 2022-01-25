b61154680ed9049ad547971b8ac4e96b98f3fb6d
Birk Brauer <3842623+bb1@users.noreply.github.com>
2022-01-25 18:09:08 +0100

    using axios

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import fetch from 'node-fetch';
+import axios from 'axios';
 import { AutoCompleteResponse } from './interfaces';
 
 const BASE_URL = 'https://geoportal.stadt-koeln.de/Finder/Lookup?filter=type:adr&query=';
